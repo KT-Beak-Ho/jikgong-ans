d97deafa5a6006a64e35e8335bc1a1ad7e78bbf6
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-06-14 15:42:19 +0900

    CompanyJoinPage2Screen navigation 추가

--- app/src/main/java/com/billcorea/jikgong/MainActivity.kt
+++ app/src/main/java/com/billcorea/jikgong/MainActivity.kt
@@ -43,8 +43,10 @@ import com.billcorea.jikgong.presentation.SplashScreen
 import com.billcorea.jikgong.presentation.WorkerLoginPage
 import com.billcorea.jikgong.presentation.WorkerProjectList
 import com.billcorea.jikgong.presentation.company.auth.join.page1.CompanyJoinPage1Screen
+import com.billcorea.jikgong.presentation.company.auth.join.page2.CompanyJoinPage2Screen
 import com.billcorea.jikgong.presentation.company.auth.join.shared.CompanyJoinSharedViewModel
 import com.billcorea.jikgong.presentation.destinations.CompanyJoinPage1ScreenDestination
+import com.billcorea.jikgong.presentation.destinations.CompanyJoinPage2ScreenDestination
 import com.billcorea.jikgong.presentation.destinations.JikgongAppDestination
 import com.billcorea.jikgong.presentation.destinations.JoinPage1Destination
 import com.billcorea.jikgong.presentation.destinations.JoinPage2Destination
@@ -135,6 +137,13 @@ class MainActivity : ComponentActivity() {
                             }
                             composable(CompanyJoinPage1ScreenDestination.route) {
                                 CompanyJoinPage1Screen(
+                                    companyJoinViewModel = companyJoinViewModel, // 기업 전용 ViewModel 전달
+                                    navigator = navigator,
+                                    modifier = Modifier.padding(5.dp)
+                                )
+                            }
+                            composable(CompanyJoinPage2ScreenDestination.route) {
+                                CompanyJoinPage2Screen(
                                     companyJoinViewModel = companyJoinViewModel, // 기업 전용 ViewModel 전달
                                     navigator = navigator,
                                     modifier = Modifier.padding(5.dp)
