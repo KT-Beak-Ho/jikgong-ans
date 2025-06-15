5b88cb7cfffd1f27ce98a6c9d7f41bad6a9be981
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-06-16 02:16:54 +0900

    minLines 파라미터 제거 및 keyboardOptions 선언방식 변경

--- app/src/main/java/com/billcorea/jikgong/presentation/company/auth/common/components/CommonTextInput.kt
+++ app/src/main/java/com/billcorea/jikgong/presentation/company/auth/common/components/CommonTextInput.kt
@@ -2,17 +2,16 @@ package com.billcorea.jikgong.presentation.company.auth.common.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.billcorea.jikgong.presentation.common.KeyboardConstants
 import com.billcorea.jikgong.ui.theme.appColorScheme
 
 @Composable
@@ -25,15 +24,11 @@ fun CommonTextInput(
   labelAppendTextColor: Color = appColorScheme.primary,
   placeholder: String = "",
   maxLines: Int = 1,
-  minLines: Int = 1,
   validationError: String? = null,
+  keyboardOptions: KeyboardOptions = KeyboardConstants.Options.DEFAULT,
+  keyboardActions: KeyboardActions = KeyboardConstants.Actions.NONE,
 ) {
-  // maxLines가 minLines보다 작은 경우 자동으로 minLines의 3배로 설정
-  val adjustedMaxLines = if (maxLines < minLines) {
-    minLines * 3
-  } else {
-    maxLines
-  }
+
   Column(modifier = modifier) {
     //  라벨
     LabelText(
@@ -47,13 +42,10 @@ fun CommonTextInput(
       value = value,
       onValueChange = onChange,
       placeholder = { Text(placeholder) },
-      keyboardOptions = KeyboardOptions(
-        keyboardType = KeyboardType.Text,
-        imeAction = ImeAction.Next
-      ),
-      maxLines = adjustedMaxLines,
-      minLines = minLines,
-      modifier = Modifier.fillMaxWidth(),
+      keyboardOptions = keyboardOptions,
+      keyboardActions = keyboardActions,
+      maxLines = maxLines,
+      modifier = modifier,
       isError = validationError != null,
       supportingText = validationError?.let {
         { Text(text = it) }
